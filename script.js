42304ed529d58e9b2aacd71eb408b216bbbff2ba
Cmoy07 <cmoyer07@comcast.net>
2025-10-20 10:12:07 -0400

    added fetch request

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 console.log("script.js loaded");
 
+let data = fetch("https://dogapi.dog/api/facts");
+console.log(data);
+
+function getDogFact() {
+  fetch("https://dogapi.dog/api/facts")
+    .then(response => response.json())  // Covert the response
+    .then(data => { 
+      // Update the DOM with the dog fact
+      const fact = data.facts[0]; 
+      const output = document.getElementById("fact-output"); 
+      output.textContent = fact; 
+    }) 
+    .catch(error => { 
+      // Message to print if there was an error
+      console.error("Error fetching dog fact:", error); 
+    });
+}
+
+// Same button code as async/await 
+const button = document.getElementById("fetch-dog-btn"); 
+
+button.addEventListener("click", function () { 
+  getDogFact(); 
+});
